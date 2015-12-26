3d4d68f667127d7b46adaf0a51bb7934d53db303
tomfin <tom.finlayson@gmail.com>
2015-12-26 17:00:45 +0000

    Added calls to service endpoint for plumbing test.

--- client/app/list/list.controller.js
+++ client/app/list/list.controller.js
@@ -1,6 +1,11 @@
 'use strict';
 
 angular.module('oshi2App')
-  .controller('ListCtrl', function ($scope) {
-	  $scope.blah = {};
+  .controller('ListCtrl', function ($scope, Games) {
+	  
+      $scope.games = [];
+      Games.all().$promise.then(function (gamesList) {
+          $scope.games = gamesList;
+      });
+
   });
---
+++ client/app/services/game.service.js
@@ -0,0 +1,13 @@
+angular.module('oshi2App')
+.factory('Games', function($resource, ENDPOINT) {
+	return $resource(ENDPOINT + 'api/open/game', {}, {
+		'all': {
+			method: 'GET',
+			isArray: true
+		}
+	});
+});
+
+
+//http://136.243.111.170:8080/api/open/game
+	
