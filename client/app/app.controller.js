4464290aae18cb04c3d5f74310dafb33bf64c40b
tomfin <tom.finlayson@gmail.com>
2015-12-29 16:37:22 +0000

    Fixed navbar buttons

--- client/app/app.controller.js
+++ client/app/app.controller.js
@@ -4,7 +4,6 @@ angular.module('oshi2App')
   .controller('AppCtrl', function ($scope, $window) {
 
 	$scope.isHome = function () { // jshint ignore:line
-		console.log('D> $window.location.pathname: ', $window.location.pathname);
 		if ($window.location.pathname === '/') {
 		    return true;
 		} else {
