4d2b98f682cf5b606079c0fd2bfaa5da88aef280
tomfin <tom.finlayson@gmail.com>
2016-01-06 15:06:31 +0000

    Re-added missing contact form.

--- client/app/help/help-contact/help-contact.controller.js
+++ client/app/help/help-contact/help-contact.controller.js
@@ -1,6 +1,21 @@
 'use strict';
 
 angular.module('oshi2App')
-  .controller('HelpContactCtrl', function ($scope) {
-	  $scope.blah = {};
+  .controller('HelpContactCtrl', function ($scope, ContactUs) {
+	  
+	$scope.contactus = function(contact) {
+		  
+		$scope.contactResponse = {};
+		  ContactUs.post(contact).then(function (res) {
+		  $scope.contactResponse = res;
+		  console.log('D> Res: ', res);
+		  $scope.contact = {};
+		  $scope.responseSuccess = 'Success';
+		  $scope.responseError = null;
+		}, function() {
+			$scope.responseSuccess = null;
+			$scope.responseError = 'Error';
+		});
+	};
+
   });
