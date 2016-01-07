2c6cfc4df2e8d0846d9a537d095aa5afc52c11ac
tomfin <tom.finlayson@gmail.com>
2016-01-07 02:42:23 +0000

    Commenting out debug.

--- client/app/services/sitedata.service.js
+++ client/app/services/sitedata.service.js
@@ -4,7 +4,7 @@ angular.module('oshi2App')
   .factory('SiteData', function ($resource, $q, ENDPOINT) {
     console.log('begin SiteData');
 
-    var timesLoadMethodCalled = 0;
+//    var timesLoadMethodCalled = 0;
     var siteData;
     var loading = false;
     var pendingPromises = [];
@@ -12,7 +12,7 @@ angular.module('oshi2App')
     return {
 
       load: function () {
-        console.log('called SiteData.load - (time ' + (++timesLoadMethodCalled) + ')');
+//        console.log('called SiteData.load - (time ' + (++timesLoadMethodCalled) + ')');
 
         var loadDeferred = $q.defer();
         pendingPromises.push(loadDeferred);
